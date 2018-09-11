331a82d70cbfa3c4f838f9c740c1596ffd886982
SangeethaMadhi <42571208+sangeethamathi@users.noreply.github.com>
2018-09-11 12:20:40 +0530

    Create year is a leap year or not.py

---
+++ year is a leap year or not.py
@@ -0,0 +1,9 @@
+year=int(input('Enter the year:'))
+if year % 4 == 0
+  print('Year,is a leap year')
+ elif year % 100 == 0:
+  print('Year,is  not a leap year')
+ elif year % 400 == 0:
+  print('Year,is a leap year')
+ else:
+  print('Year,is not a leap year')
