133a3a530a6c7e8967d9395d0e476e42a9ad96d0
SangeethaMadhi <42571208+sangeethamathi@users.noreply.github.com>
2018-09-12 14:51:18 +0530

    Create arithmetic progression.py

---
+++ arithmetic progression.py
@@ -0,0 +1,14 @@
+num1=int(input('Enter first num'))
+num2=int(input('Enter second(num'))
+add=num1+num2
+sub=num1-num2
+mul=num1*num2
+div=num1/num2
+power=num1**numnum2
+modulus=num1%num2
+print('sum of num1 and num2')
+print('sub of num1 and num2')
+print('mul of num1 and num2')
+print('div of num1 and num2')
+print('power of num1 and num2')
+print('modulus of num1 and num2')
