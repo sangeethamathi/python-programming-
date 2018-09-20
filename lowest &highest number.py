4a62533cbd6a11a1129783c00ea7003cfb08d839
SangeethaMadhi <42571208+sangeethamathi@users.noreply.github.com>
2018-09-20 16:37:29 +0530

    Create lowest &highest number.py

---
+++ lowest &highest number.py
@@ -0,0 +1,6 @@
+num=int(input('Enter the numbers:')
+for n in range(num):
+numbers=int(input('Enter numbers'))
+1st.append(numbers)
+print('largest element in the array is:')
+print('minimum element in the array is:')
