f0430e7aa557e78e232606f1876cb5b235be7496
SangeethaMadhi <42571208+sangeethamathi@users.noreply.github.com>
2018-09-20 17:27:49 +0530

    Create print in words.py

---
+++ print in words.py
@@ -0,0 +1,21 @@
+def ten(num):
+if(num[0]==1):
+ word=one
+if(num[0]==2):
+ word=two
+if(num[0]==3):
+ word=three
+if(num[0]==4):
+ word=four
+if(num[0]==5):
+ word=five
+if(num[0]==6):
+ word=six
+if(num[0]==7):
+ word=seven
+if(num[0]==8):
+ word=Eight
+if(num[0]==9):
+word=word+text
+word=word.strip()
+return word
