1bc83a80975a838219be55266b1cda6b445ddf1c
SangeethaMadhi <42571208+sangeethamathi@users.noreply.github.com>
2018-09-20 16:40:59 +0530

    Create Average number.py

---
+++ Average number.py
@@ -0,0 +1,7 @@
+num=int(input('Enter the number:')
+total_sum=0
+for n in range(num):
+numbers_float(input('Enter number:'))
+total_sum+=numbers
+avg=total=sum/num
+print('Average of num, number is:',avg)
