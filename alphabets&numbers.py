1fbd3883202f2eec25ae8872722ee8fc243da746
SangeethaMadhi <42571208+sangeethamathi@users.noreply.github.com>
2018-09-20 17:53:32 +0530

    Create alphabets&numbers.py

---
+++ alphabets&numbers.py
@@ -0,0 +1,7 @@
+string=raw_input('Enter string:')
+count1=0
+count2=0
+count1=count1+1
+count2=count2+1
+ print('The number of digits:')
+ print('The number of characters:')https://github.com/sangeethamathi/python-programming-/blob/master/two%20numbers%20is%20even%20or%20add.py
