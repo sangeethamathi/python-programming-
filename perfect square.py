61de20d0dabd05bda4758858d4b94506f87bac35
SangeethaMadhi <42571208+sangeethamathi@users.noreply.github.com>
2018-09-26 17:16:13 +0530

    Update perfect square.py

--- perfect square.py
+++ perfect square.py
@@ -2,4 +2,4 @@ n=int(input('Enter two numbers'))
 if(n==i*i):
 print('Perfect square')
 else:
-print('Not perfect square')https://github.com/sangeethamathi/python-programming-/blob/master/multiples%20of%2013.py
+print('Not perfect square')
