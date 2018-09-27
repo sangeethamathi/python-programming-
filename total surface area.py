550a12df3b898be1e1ad49cece85f731ddf17ac5
SangeethaMadhi <42571208+sangeethamathi@users.noreply.github.com>
2018-09-27 16:55:02 +0530

    Create total surface area.py

---
+++ total surface area.py
@@ -0,0 +1,9 @@
+length=float(input('Enter the length of a cuboid'))
+width=float(input('Enter the width of a cuboid'))
+height=float(input('Enter the height of a cuboid'))
+surface area=2*(length*width+length*height+width*height)
+volume=length*width*height
+lateral surface area=2*height*(length+width)
+print('surface area of cuboid')
+print('The volume of cuboid')
+print('The lateral surface area of cuboid')
