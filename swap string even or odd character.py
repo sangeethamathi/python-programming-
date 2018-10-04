32d5a1346e4b54ebfdd61ddfdcc8dd73425dbdbc
SangeethaMadhi <42571208+sangeethamathi@users.noreply.github.com>
2018-10-04 17:15:29 +0530

    https://github.com/sangeethamathi/python-programming-/blob/master/isomorphic.py

---
+++ swap string even or odd character.py
@@ -0,0 +1,8 @@
+string1=input('Enter the first string swap')
+if(string1==x):
+else:
+ string2=input('Enter the second string swap')
+ if(check==0):
+  print('It is an even number')
+if(check==1):
+ print('It is an odd number')https://github.com/sangeethamathi/python-programming-/blob/master/isomorphic.pyhttps://github.com/sangeethamathi/python-programming-/blob/master/isomorphic.py
