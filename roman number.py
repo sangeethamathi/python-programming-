2e82407e762388cba4a42574c52557e253f9ff8c
SangeethaMadhi <42571208+sangeethamathi@users.noreply.github.com>
2018-10-04 17:02:24 +0530

    Create roman number.py

---
+++ roman number.py
@@ -0,0 +1,16 @@
+def write_roman(num):
+roman[1000]='M'
+roman[900]='CM'
+roman[500]='D'
+roman[400]='CD'
+roman[100]='C'
+roman[90]='XC'
+roman[50]='L'
+roman[40]='XL'
+roman[10]='X'
+roman[9]='IX'
+roman[5]='V'
+roman[4]='IV'
+roman[1]='I'
+if(num>0):
+ roman_num(num)
