dcdee18e58dc7dd220ca6a351587c2659039d0ad
SangeethaMadhi <42571208+sangeethamathi@users.noreply.github.com>
2018-12-01 13:26:02 +0530

    Create number of string.py

---
+++ number of string.py
@@ -0,0 +1,9 @@
+string1=raw_input('Enter first string:')
+string2=raw_input('Enter second string:')
+count1=0
+count2=0
+print('Largest string is:')
+print(string2)
+else:
+print('Largest string is:')
+print(string1)
