b51c9dbf498426abbb9d85fd6e305b18122acaab
SangeethaMadhi <42571208+sangeethamathi@users.noreply.github.com>
2019-01-04 14:12:20 +0530

    Create lowercase uppercse.py

---
+++ lowercase uppercse.py
@@ -0,0 +1,12 @@
+print('Enter x')
+string=input('Enter any string to convert uppercase:')
+ if(str='x')
+ else:
+  sting in uppercase=string.upper()
+  print('string in uppercase=string in uppercase:')
+print('Enter x')
+string=input('Enter any string to convert lowercase:')
+ if(str='x')
+ else:
+  string in lowercase=string.lower()
+  print('string in lowercase=string in lowercase:')
