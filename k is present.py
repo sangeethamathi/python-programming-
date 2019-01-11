43e0619da4a7f0032b628568beeda155853e17c5
SangeethaMadhi <42571208+sangeethamathi@users.noreply.github.com>
2019-01-11 10:46:56 +0530

    Create k is present.py

---
+++ k is present.py
@@ -0,0 +1,12 @@
+list1 =[10, 20, 30, 40, 50, 60] 
+val = 5
+if(check(list1, val)): 
+    print"Yes"
+else: 
+    print"No"
+  
+val = 20 
+if(check(list1, val)): 
+    print"Yes"
+else: 
+    print"No"
